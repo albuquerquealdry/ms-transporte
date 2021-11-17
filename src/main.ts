ff0baea3c6b8b6c2c520afce1d92c0a64f2cf81c
Aldry <aldry.albuquerque@telefonica.com>
2021-11-17 11:58:23 -0300

    Implementação 2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+var cors = require('cors')
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
+  app.use(cors())
+  
 }
 bootstrap();
