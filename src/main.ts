3952292f751dbcc54533284fbda7dfb10284b8e2
Aldry <aldry.albuquerque@telefonica.com>
2021-11-19 08:52:01 -0300

    swegger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 var cors = require('cors')
 
@@ -6,6 +7,14 @@ var cors = require('cors')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('Delivery Api')
+    .setDescription('The delivery API description')
+    .setVersion('1.0')
+    .addTag('delivery')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.enableCors()
   await app.listen(process.env.PORT || 3000);
   
